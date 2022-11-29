c7f75db7cc41e83f9f387a3344b2fdaf5ec35179
EralChen <eralchen@foxmail.com>
2022-11-29 17:27:54 +0800

    login

--- app/src/api/_request/base.ts
+++ app/src/api/_request/base.ts
@@ -1,18 +1,26 @@
 import { RestFetch } from '@vunk/skzz/shared/utils-fetch'
 import type { R } from '@vunk/skzz/shared'
 import { usePlatformStore } from '@/stores/platform'
+import { AnyFunc } from '@vunk/core'
 
 export const restFetch = new RestFetch({
   baseURL: '',
   DEV: import.meta.env.DEV,
 })
 
-export const request = async <T>(...args: Parameters<typeof restFetch.request>) => {
-  const platformStore = usePlatformStore()
-  const platform = platformStore.getPlatformInfo()
-  restFetch.baseURL = platform.url
+export function withPlatform <T extends AnyFunc> (fn: T) {
+  return (async (...args) => {
+    const platformStore = usePlatformStore()
+    const platform = platformStore.getPlatformInfo()
+    restFetch.baseURL = platform.url
+    return fn(...args)
+  }) as T
+}
 
+const baseRequest = async <T>(...args: Parameters<typeof restFetch.request>) => {
   const data = await restFetch.request<R<T>>(...args)
-
   return data 
 }
+
+export const request = withPlatform(baseRequest)
+
--- app/src/api/login/index.ts
+++ app/src/api/login/index.ts
@@ -1,16 +1,6 @@
-import { usePlatformStore } from '@/stores/platform'
-import { restFetch } from '@/api/_request/base'
+import { withPlatform } from '@/api/_request/base'
 import * as baseLogin from './base'
-import { AnyFunc } from '@vunk/core'
 
-function withPlatform <T extends AnyFunc> (fn: T) {
-  return (async (...args) => {
-    const platformStore = usePlatformStore()
-    const platform = platformStore.getPlatformInfo()
-    restFetch.baseURL = platform.url
-    return fn(...args)
-  }) as T
-}
 
 export const rCaptcha = withPlatform(baseLogin.rCaptcha)
 export const rUserInfo = withPlatform(baseLogin.rUserInfo)
