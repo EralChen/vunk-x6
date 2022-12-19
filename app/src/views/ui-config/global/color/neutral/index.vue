f9a9689ec3ea996256fc2ca69eccbf46ff729450
EralChen <eralchen@foxmail.com>
2022-12-19 10:36:06 +0800

    fill color

--- app/src/views/ui-config/global/color/neutral/index.vue
+++ app/src/views/ui-config/global/color/neutral/index.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
 import TextVue from './text.vue'
 import BorderVue from './border.vue'
+import FillVue from './fill.vue'
+
 </script>
 <template>
   <TextVue v-bind="$attrs"></TextVue>
   <BorderVue v-bind="$attrs"></BorderVue>
+  <FillVue v-bind="$attrs"></FillVue>
 </template>
