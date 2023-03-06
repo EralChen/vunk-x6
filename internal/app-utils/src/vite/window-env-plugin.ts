7b48f205fca413cbd1a42fd3a0c381dfb07e0dbb
EralChen <eralchen@foxmail.com>
2023-03-06 15:03:07 +0800

    clear

--- internal/app-utils/src/vite/window-env-plugin.ts
+++ internal/app-utils/src/vite/window-env-plugin.ts
@@ -20,6 +20,10 @@ export function windowEnvPlugin (): PluginOption {
       return code
     },
     transformIndexHtml (html) {
+      const obj = getEnv(mode)
+      Reflect.deleteProperty(obj, 'NODE_ENV')
+      Reflect.deleteProperty(obj, 'VITE_BASE_URL')
+      
       return html.replace(`<script id="env"></script>`,
         `<script id="env">
           ${'window.__env__ =' + JSON.stringify(getEnv(mode), null, 2)}
--- typings/app.d.ts
+++ typings/app.d.ts
@@ -1,4 +1,10 @@
 interface ImportMetaEnv {
+  /**
+   * 资源公共路径
+   */
   VITE_BASE_URL: string
+  /**
+   * 接口统一地址
+   */
   VITE_BASE_API: string
 }
