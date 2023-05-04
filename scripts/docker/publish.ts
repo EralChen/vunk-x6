957e1ed0c46aaad9c94b0ca2783ef4df65530c4a
EralChen <eralchen@foxmail.com>
2023-05-04 15:27:45 +0800

    docker:publish

---
+++ scripts/docker/publish.ts
@@ -0,0 +1,26 @@
+import { workRoot } from '@lib-env/path'
+import { taskWithName, run } from '@lib-env/shared'
+import { series } from 'gulp'
+import mri from 'mri'
+
+interface MriData {
+  v: string // aaa-bbb
+}
+
+const argv = process.argv.slice(2)
+const mriData = mri<MriData>(argv)
+if (!mriData.v) {
+  mriData.v = 'latest'
+}
+export default series([
+  taskWithName('docker build', async () => {
+    await run(`docker build -t zz-platform-config:${mriData.v} .`, workRoot)
+  }),
+  taskWithName('docker tag', async () => {
+    await run(`docker tag zz-platform-config:${mriData.v} t1.zjsophon.com:58088/zz-platform-config:${mriData.v}`, workRoot)
+  }),
+  taskWithName('docker push', async () => {
+    await run(`docker push t1.zjsophon.com:58088/zz-platform-config:${mriData.v}`, workRoot)
+  }),
+ 
+])
