8c318dc4cd6c431df9055f5ed7b73291a20d55ff
EralChen <eralchen@foxmail.com>
2023-05-06 14:25:13 +0800

    @skzz-platform/composables

--- app/src/stores/theme/index.ts
+++ app/src/stores/theme/index.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { useTheme } from '@skzz-platform/composables/theme'
+import { useTheme } from '@skzz-platform/composables'
 export const useThemeStore = defineStore('theme', () => {
   const theme = useTheme('ZZPT_ADMIN')
   return theme
