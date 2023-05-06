ed0dbed91c5bec60e948775c10afa6ef45584124
EralChen <eralchen@foxmail.com>
2023-05-06 16:05:07 +0800

    update env

--- app/src/styles/index.ts
+++ app/src/styles/index.ts
@@ -5,11 +5,5 @@ import '@vunk/core/index.css'
 import '@vunk/form/index.css'
 import './form'
 import './layout/index.css'
-import '@zzg6/flow/index.css'
 import '@skzz/platform/index.css'
 
-// for formaker
-import 'mapbox-gl/dist/mapbox-gl.css'
-import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
-import '@vumap/core/index.css'
-import '@vunk/formaker/index.css'
