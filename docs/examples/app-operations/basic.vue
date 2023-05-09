cc9ac16eb9c02ea6b783bb10e5b77db515403863
EralChen <eralchen@foxmail.com>
2023-05-09 13:35:16 +0800

    clear

--- docs/examples/app-operations/basic.vue
+++
@@ -1,6 +0,0 @@
-<script lang="ts" setup>
-import { SkAppOperations } from '@skzz-template/components/app-operations'
-</script>
-<template>
-  <SkAppOperations></SkAppOperations>
-</template>
--- docs/examples/css-color-picker/basic.vue
+++
@@ -1,6 +0,0 @@
-<script lang="ts" setup>
-import { SkCssColorPicker } from '@skzz-template/components/css-color-picker'
-</script>
-<template>
-  <SkCssColorPicker :label="'颜色选择'"></SkCssColorPicker>
-</template>
