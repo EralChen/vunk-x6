6cd2bba1faba7d4bd6018e560a70836255c12498
EralChen <eralchen@foxmail.com>
2023-05-11 16:04:08 +0800

    application

--- packages/api/platform/application/index.ts
+++ packages/api/platform/application/index.ts
@@ -191,6 +191,7 @@ export const cBoundApplications = (
     },
   }, {
     loading: true,
+    msg: '绑定成功',
   })
 }
 
@@ -221,6 +222,7 @@ export const dBoundApplications = (data: Partial<BoundApplication>[]) => {
     },
   }, {
     loading: true,
+    msg: '解绑成功',
   })
 }
 
