e476d0023b0d846ef7eda05160eaf2d419fd64c0
EralChen <eralchen@foxmail.com>
2023-05-11 13:50:41 +0800

    ElScrollbar

--- app/src/components/LayoutTop/index.vue
+++ app/src/components/LayoutTop/index.vue
@@ -6,6 +6,7 @@ import { ElAvatar } from 'element-plus'
 import { logout } from '@skzz-platform/api/login'
 import ApplicationCascader from '_c/ApplicationCascader/index.vue'
 import SizeCtrl from '_c/SizeCtrl/index.vue'
+import { VkDuplex } from '@vunk/core'
 defineEmits({
   'load': null,
 })
@@ -14,56 +15,68 @@ const { layoutTopClassName } = useThemeStore()
 
 </script>
 <template>
-
-    <div sk-flex="row-between-center" class="layout-top text-white"
-      :class="{
-        [layoutTopClassName]: true
-      }"
-    >
-      <div sk-flex="row_center">
-        <div class="layout-top-title f-xxxl font-600 plr-l ptb-s">
-          智子开发平台
+  <VkDuplex :direction="'row'" sk-flex="row-between-center" :withFlex="'one'" class="layout-top text-white"
+    :class="{
+      [layoutTopClassName]: true
+    }"
+  >
+    <template #one>
+      <VkDuplex :direction="'row'" sk-flex="row_center">
+        <template #one>
+          <div class="layout-top-title f-xxxl font-600 plr-l ptb-s">
+            智子开发平台
         </div>
-        <LayoutTopMenu
-          @load="$emit('load')"
-        ></LayoutTopMenu>
-      </div>
-      
+        </template>
+
+
+        <template #two>
+          <ElScrollbar>
+            <LayoutTopMenu
+              @load="$emit('load')"
+            ></LayoutTopMenu>
+          </ElScrollbar>
+
+        </template>
+
+      </VkDuplex>
+
+    </template>
+
+    <template #two>
       <div class="pr-l sub-ml-widget-space " sk-flex="row_center">
-        
-        <DarkSwitch></DarkSwitch>
-                
-        <SizeCtrl></SizeCtrl>
-
-        <ApplicationCascader
-          :size="32"
-        ></ApplicationCascader>
-
-        <ElDropdown>
-          <ElAvatar
-            :size="32"
-            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-          >
-            user
-          </ElAvatar>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <el-dropdown-item @click="logout">
-                <ElLink :type="'danger'" >退出登录</ElLink>
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-   
-        </ElDropdown>
-
       
-        <!-- <ElButton @click="logout">退出</ElButton> -->
+      <DarkSwitch></DarkSwitch>
+              
+      <SizeCtrl></SizeCtrl>
 
-      </div>
+      <ApplicationCascader
+        :size="32"
+      ></ApplicationCascader>
+
+      <ElDropdown>
+        <ElAvatar
+          :size="32"
+          src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+        >
+          user
+        </ElAvatar>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item @click="logout">
+              <ElLink :type="'danger'" >退出登录</ElLink>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+  
+      </ElDropdown>
+
+    
+      <!-- <ElButton @click="logout">退出</ElButton> -->
 
     </div>
+    </template>
 
-
+  </VkDuplex>
 </template>
 <style>
 .layout-top{
