3b8fee4dbbd90854b6189300ab12a23f408492b7
EralChen <eralchen@foxmail.com>
2023-05-12 18:09:31 +0800

    manualChunks

--- internal/app-utils/src/vite/manual-chunks.ts
+++ internal/app-utils/src/vite/manual-chunks.ts
@@ -1,19 +1,7 @@
 import type { ManualChunksOption } from 'rollup'
-export const manualChunks: ManualChunksOption  = (id, { getModuleInfo }) => {
+export const manualChunks: ManualChunksOption  = (id) => {
   if (id.includes('@arcgis/core')) {
     return 'esri'
   }
-  const commonRegs = [ 
-    /(.*)\/src\/components\/(.*)/,
-    /(.*)@skzz-(.*)/,
-  ]
-  for (const reg of commonRegs) {
-    if (reg.test(id)) {
-      const importersLen = getModuleInfo(id)?.importers.length ?? 0
-      // 被多处引用
-      if (importersLen > 1) {
-        return 'app-common'
-      }
-    }
-  }
+
 }
