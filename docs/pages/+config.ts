57e58390ee619f7f700f4b91b5abc1e31b37e837
EralChen <eralchen@foxmail.com>
2025-03-03 15:49:33 +0800

    pref: update env

--- docs/pages/+config.ts
+++ docs/pages/+config.ts
@@ -1,8 +1,8 @@
-import Layout from '#s/layouts/default/index.vue'
 import type { Config } from 'vike/types'
+import Layout from '#s/layouts/default/index.vue'
 // const Layout = 'import:#s/layouts/default/index.vue:default'
-
 
 export default {
   Layout,
+  prerender: true,
 } satisfies Config
--- docs/vite.config.mts
+++ docs/vite.config.mts
@@ -68,9 +68,7 @@ export default defineConfig(async ({ mode }) => {
     plugins: [
       vueDevTools(),
 
-      vike({
-        prerender: true,
-      }),
+      vike(),
 
       vue({
         include: [/\.vue$/, /\.md$/],
